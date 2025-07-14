e6126f2310ef66efe5e1669e9124d5fd79d08b96
Carlos.Arroyo <carlos23_adrian@hotmail.com>
2025-07-14 10:13:34 -0600

    login arreglado

--- src/modules/auth/views/LoginView.vue
+++ src/modules/auth/views/LoginView.vue
@@ -1,25 +1,24 @@
 <template>
-  <div class=""></div>
   <form @submit.prevent="">
-    <h1 class="text-2xl font-semibold mb-4 content-center">Inciar Sesion</h1>
-    <img src="@/assets/login/uanl-logo-v-color.png" class="img w-10 z-10" />
-    <!-- email Input -->
+    <div class="flex flex-row gap-36">
+      <h1 class="text-2xl font-semibold mb-4 content-center">Inciar Sesion</h1>
+      <img src="@/assets/login/uanl-logo-v-color.png" class="img w-10" />
+    </div>
+
     <div class="mb-4">
       <fieldset class="fieldset">
         <legend class="fieldset-legend">Usuario</legend>
         <input type="text" class="input" placeholder="" />
-        <!-- <p class="label">Optional</p> -->
       </fieldset>
     </div>
-    <!-- Password Input -->
+
     <div class="mb-4">
       <fieldset class="fieldset">
         <legend class="fieldset-legend">Contraseña</legend>
         <input type="text" class="input" placeholder="" />
-        <!-- <p class="label">Optional</p> -->
       </fieldset>
     </div>
-    <!-- Remember Me Checkbox -->
+
     <div class="mb-4 flex items-center">
       <fieldset class="fieldset bg-base-100 border-base-300 rounded-box w-64 border p-4">
         <legend class="fieldset-legend">Opciones de Inicio</legend>
@@ -29,19 +28,11 @@
         </label>
       </fieldset>
     </div>
-    <!-- Forgot Password Link -->
+
     <div class="mb-6">
       <RouterLink class="link" to="recuperar"> ¿Olvidaste la contraseña? </RouterLink>
-      <!-- <a href="#" class="link">Olvidaste la contraseña?</a> -->
     </div>
-    <!-- Login Button -->
+
     <button class="btn btn-block">Ingresar</button>
   </form>
-  <!-- Sign up  Link -->
 </template>
-
-<style>
-.img {
-  object-fit: contain;
-}
-</style>
--- src/modules/auth/views/RecuperarView.vue
+++ src/modules/auth/views/RecuperarView.vue
@@ -1,31 +1,54 @@
 <template>
-  <div class="w-full">
+  <!-- <div class="w-full">
     <h1 class="text-2xl font-semibold mb-4">Restaurar contraseña</h1>
     <form @submit.prevent="">
-      <!-- email Input -->
       <div class="mb-4">
         <fieldset class="fieldset">
           <legend class="fieldset-legend">Usuario</legend>
           <input type="text" class="input" placeholder="" />
-          <!-- <p class="label">Optional</p> -->
+          <p class="label">Optional</p>
         </fieldset>
       </div>
-      <!-- Password Input -->
+
       <div class="mb-4">
         <fieldset class="fieldset">
           <legend class="fieldset-legend">Correo Electronico</legend>
           <input type="text" class="input" placeholder="" />
-          <!-- <p class="label">Optional</p> -->
         </fieldset>
       </div>
 
-      <!-- Forgot Password Link -->
       <div class="mb-6">
         <RouterLink class="link" to="login"> Login </RouterLink>
-        <!-- <a href="#" class="link">Olvidaste la contraseña?</a> -->
       </div>
-      <!-- Login Button -->
+
       <button class="btn btn-block">Enviar Solicitud</button>
     </form>
+  </div> -->
+  <div class="flex flex-col">
+    <h1 class="text-2xl font-semibold mb-4">Restaurar contraseña</h1>
+    <div>
+      <form @submit.prevent="">
+        <div class="mb-4">
+          <fieldset class="fieldset">
+            <legend class="fieldset-legend">Usuario</legend>
+            <input type="text" class="input" placeholder="" />
+            <!-- <p class="label">Optional</p> -->
+          </fieldset>
+        </div>
+
+        <div class="mb-4">
+          <fieldset class="fieldset">
+            <legend class="fieldset-legend">Correo Electronico</legend>
+            <input type="text" class="input" placeholder="" />
+          </fieldset>
+        </div>
+
+        <div class="mb-6">
+          <RouterLink class="link" to="login"> Login </RouterLink>
+        </div>
+
+        <button class="btn btn-block">Enviar Solicitud</button>
+      </form>
+    </div>
   </div>
 </template>
